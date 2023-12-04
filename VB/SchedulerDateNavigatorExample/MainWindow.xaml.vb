2ef9de011d8fbacdbbad8492849e86792a979538
DevExpressExampleBot <fakeEmail>
2023-12-04 17:29:49 +0400

    Source auto update [skip ci]

--- VB/SchedulerDateNavigatorExample/MainWindow.xaml.vb
+++ VB/SchedulerDateNavigatorExample/MainWindow.xaml.vb
@@ -1,24 +1,23 @@
-﻿Imports DevExpress.Xpf.Scheduling
-Imports System
-Imports System.Windows
+Imports DevExpress.Xpf.Scheduling
 
 Namespace SchedulerDateNavigatorExample
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits DevExpress.Xpf.Core.ThemedWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
-        #Region "#CustomizeSpecialDates"
+#Region "#CustomizeSpecialDates"
         Private Sub SchedulerDateNavigatorStyleSettings_CustomizeSpecialDates(ByVal sender As Object, ByVal e As CustomizeSpecialDatesEventArgs)
             e.SpecialDates.Add(Date.Today.AddDays(-2))
             e.SpecialDates.Add(Date.Today.AddDays(-4))
             e.SpecialDates.Add(Date.Today.AddDays(2))
         End Sub
-        #End Region ' #CustomizeSpecialDates
+#End Region  ' #CustomizeSpecialDates
     End Class
 End Namespace
